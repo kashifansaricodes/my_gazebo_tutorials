5c0a84d181f3ab8793b9beed9c2dd7a3e7b937a5
kashifansaricodes <kansari@umd.edu>
2024-11-24 21:27:30 -0500

    adding cpplint and clangtidy

--- build/walker/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kashif/SDR/temp3/my_gazebo_tutorials/walker
-# Build directory: /home/kashif/SDR/temp3/my_gazebo_tutorials/build/walker
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
